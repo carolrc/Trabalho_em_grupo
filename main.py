128e737f7a8e48541a3873fbf591bc1d1f6035c1
PJ - Data Analytics 2024.1 <3668822024.1@senacrjedu.local>
2024-04-10 11:00:02 -0300

    subindo a lógica com as perguntas

--- main.py
+++ main.py
@@ -1,6 +1,60 @@
-def menu(){
-    cadastrar = input("DESEJA CADASTRAR?\n[S]SIM\n[N]NÃO")
-    if cadastar == "S":
-        idade = int(input("Digite a idade do usuário"))
-        genero = int(input("Digite o gênero do usuário"))
-}
+def menu():
+    idade=''
+    while True: 
+        idade = input('Qual a sua idade?')
+        if idade != '00':
+            genero = input('Qual o seu gênero?')
+
+            print('''
+                ======================================
+                        PESQUISA DE SEGUROS
+                ======================================
+                ''')
+            
+            resposta_1=''
+            resposta_2=''
+            resposta_3=''
+            resposta_4=''
+            
+            while resposta_1 != 'sim' and resposta_1 != 'não' and resposta_1 != 'não sei':
+                resposta_1 = input('''
+                    =============================
+                            PERGUNTA 1
+                    =============================
+                    Deve ser respondido com sim/não/não sei
+                    ''')
+            
+            while resposta_2 != 'sim' and resposta_2 != 'não' and resposta_2 != 'não sei':
+                resposta_2 = input('''
+                    =============================
+                            PERGUNTA 2
+                    =============================
+                    Deve ser respondido com sim/não/não sei
+                    ''')
+
+            while resposta_3 != 'sim' and resposta_3 != 'não' and resposta_3 != 'não sei':
+                resposta_3 = input('''
+                    =============================
+                            PERGUNTA 3
+                    =============================
+                    Deve ser respondido com sim/não/não sei
+                    ''')   
+
+            while resposta_4 != 'sim' and resposta_4 != 'não' and resposta_4 != 'não sei':
+                resposta_4 = input('''
+                    =============================
+                            PERGUNTA 4
+                    =============================
+                    Deve ser respondido com sim/não/não sei
+                    ''')
+        else:
+            print('''
+                  ===================================
+                    VOCÊ ENCERROU A COLETA DE DADOS
+                  ===================================
+                  ''')
+            break
+
+        print(idade, genero, resposta_1, resposta_2, resposta_3, resposta_4)   
+
+menu()
