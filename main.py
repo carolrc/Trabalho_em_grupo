95447329e7f1c4f2cf805d98e63f69506fa31dae
Diego Fernando <diegoreis.dvlopr@gmail.com>
2024-04-11 11:34:01 -0300

    subindo a classe que identifica cada usuário por data e hora

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+lista = []
+
+class Pesquisa:
+    def __init__(self, idade, genero, resposta_1, resposta_2, resposta_3, resposta_4, data_hora):
+        self.idade = idade
+        self.genero = genero
+        self.resposta_1 = resposta_1
+        self.resposta_2 = resposta_2
+        self.resposta_3 = resposta_3
+        self.resposta_4 = resposta_4
+        self.data_hora = data_hora
+
+    def armazenar(self):
+        lista.append([self.data_hora, self.idade, self.genero, self.resposta_1, self.resposta_2,self.resposta_3,self.resposta_4])
+       
+    
+
+
 def menu():
     idade=''
     while True: 
@@ -16,43 +34,57 @@ def menu():
             resposta_3=''
             resposta_4=''
             
-            while resposta_1 != 'sim' and resposta_1 != 'não' and resposta_1 != 'não sei':
+            while resposta_1 != '1' and resposta_1 != '2' and resposta_1 != '3':
                 resposta_1 = input('''
                     =============================
                             PERGUNTA 1
                     =============================
-                    Deve ser respondido com sim/não/não sei
+                    1 - Sim
+                    2 - Não
+                    3 - Não sei          
                     ''')
             
-            while resposta_2 != 'sim' and resposta_2 != 'não' and resposta_2 != 'não sei':
+            while resposta_2 != '1' and resposta_2 != '2' and resposta_2 != '3':
                 resposta_2 = input('''
                     =============================
                             PERGUNTA 2
                     =============================
-                    Deve ser respondido com sim/não/não sei
+                    1 - Sim
+                    2 - Não
+                    3 - Não sei
                     ''')
 
-            while resposta_3 != 'sim' and resposta_3 != 'não' and resposta_3 != 'não sei':
+            while resposta_3 != '1' and resposta_3 != '2' and resposta_3 != '3':
                 resposta_3 = input('''
                     =============================
                             PERGUNTA 3
                     =============================
-                    Deve ser respondido com sim/não/não sei
+                    1 - Sim
+                    2 - Não
+                    3 - Não sei
                     ''')   
 
-            while resposta_4 != 'sim' and resposta_4 != 'não' and resposta_4 != 'não sei':
+            while resposta_4 != '1' and resposta_4 != '2' and resposta_4 != '3':
                 resposta_4 = input('''
                     =============================
                             PERGUNTA 4
                     =============================
-                    Deve ser respondido com sim/não/não sei
+                    1 - Sim
+                    2 - Não
+                    3 - Não sei
                     ''')
+        
         else:
             print('''
-                  ===================================
+                ===================================
                     VOCÊ ENCERROU A COLETA DE DADOS
-                  ===================================
-                  ''')
-            break  
+                ===================================
+                ''')
+            break
+
+        entrevistado = Pesquisa(idade, genero, resposta_1, resposta_2, resposta_3, resposta_4, 'código data e hora')
+        entrevistado.armazenar()
 
 menu()
+
+print(lista)
